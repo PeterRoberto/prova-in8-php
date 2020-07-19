382a9cf80053941a436dba59e9b873bbd497c0e2
Peter Roberto <peterrobertoweb@gmail.com>
2020-07-18 22:46:01 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,20 @@
-
-
-
 <!DOCTYPE html>
 
 <html lang="en">
-    <head>
-        <title>Prova prática - IN8 DEV</title>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link href="css/bootstrap.min.css" rel="stylesheet" />
-        <link href="css/fontawesome-free-5.13.1-web/css/all.css" rel="stylesheet" />
-        <link rel="icon" type="image/png" href="assets/imagens/favicon.png" />
+<head>
+    <title>Prova prática - IN8 DEV</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/fontawesome-free-5.13.1-web/css/all.css" rel="stylesheet" />
+    <link rel="icon" type="image/png" href="assets/imagens/favicon.png" />
+    <link rel="stylesheet" href="css/style.css" />
+</head>
 
-        
-
-        <link rel="stylesheet" href="css/style.css" />
-    </head>
- 
-    <body> 
-        <div class="overlay"></div>
-        <?php include('header.php'); ?>
-        <?php include('modulos/banner.php'); ?>
-        <?php include('modulos/formulario.php'); ?>
-        <?php include('modulos/tabela.php'); ?>
-        <?php include('footer.php'); ?>
-    </body>
- 
-</html> 
+<body> 
+    <div class="overlay"></div>
+    <?php include('header.php'); ?>
+    <?php include('modulos/banner.php'); ?>
+    <?php include('modulos/formulario.php'); ?>
+    <?php include('modulos/tabela.php'); ?>
+    <?php include('footer.php'); ?>
